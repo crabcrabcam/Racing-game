6d4589ee9fd50a4c62dc6bef91fd695fff77bb30
crabcrabcam <cam12win@gmail.com>
2016-10-04 19:33:42 +0100

    Finished game
    Could add some extra stuff like a timer or something but I probably will
    leave this as is it because I'm lazy and can't be bothered to release
    the update!

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public string startLevel;
+
+    public void NewGame()
+    {
+        Application.LoadLevel(startLevel);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float moveSpeed;
+    public float handling;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+	}
+
+    void Movement()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0.5f)
+        {
+            transform.Rotate(Vector3.back * Time.deltaTime * handling);
+        }
+        if (Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * handling);
+        }
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+        }
+    }
+}
